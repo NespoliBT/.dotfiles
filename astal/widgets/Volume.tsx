c43dc0fb6f87d488e4057267ff5173a46144a1e4
NespoliBT <40525058+NespoliBT@users.noreply.github.com>
2025-12-28 16:37:08 +0100

    volume

--- astal/widgets/Volume.tsx
+++ astal/widgets/Volume.tsx
@@ -3,19 +3,26 @@ import { timeout, Variable, bind } from "astal";
 
 const Volume = (monitor = 0) => {
   const { TOP, LEFT, RIGHT, BOTTOM } = Astal.WindowAnchor;
+  const speaker = Wp.get_default()?.audio.defaultSpeaker!;
+  const isOpen = Variable(false);
 
-  const isOpen = Variable(true);
+  speaker.subscribe(() => {
+    isOpen.set(true);
+    timeout(2000).then(() => {
+      isOpen.set(false);
+    });
+  });
 
   return (
     <window
-      className="Notification"
+      className="Volume"
       gdkmonitor={monitor}
       exclusivity={Astal.Exclusivity.NONE}
       anchor={BOTTOM | LEFT | RIGHT}
     >
       <revealer
         transitionType={Gtk.RevealerTransitionType.SLIDE_UP}
-        revealChild={isOpen.get()}
+        revealChild={bind(isOpen).as((open) => open)}
         className="revealer"
       >
         asd
